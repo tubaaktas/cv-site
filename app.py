4c83a4d03f6641f998c7873e36d4cc7d5baca0bf
Tugba Aktas <tugba@artifactsystems.com>
2024-12-04 23:42:21 +0300

    UPDATE

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def home():
     return render_template("index.html", profile=profile, experience=experience, education=education, skills=skills)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
